598169a28fe44af0ff454e91c2325273f4fdfd0e
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-23 17:04:20 +0100

    style: rename variable replaced to html

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const htmlOutput = document.getElementById('html-output');
 const preview = document.getElementById('preview');
 
 const convertMarkdown = () => {
-    let replaced = markdownInput.value;
+    let html = markdownInput.value;
     // heading
-    replaced = replaced.replace(/^(#+) (.+)$/gm, (_, level, text) => `<h${level.length}>${text}</h${level.length}>`);
+    html = html.replace(/^(#+) (.+)$/gm, (_, level, text) => `<h${level.length}>${text}</h${level.length}>`);
     // strong
-    replaced = replaced.replace(/(\*\*|__)(.+)\1/gm, (_, __, text) => `<strong>${text}</strong>`);
+    html = html.replace(/(\*\*|__)(.+)\1/gm, (_, __, text) => `<strong>${text}</strong>`);
     // em
-    replaced = replaced.replace(/(\*|_)(.+)\1/gm, (_, __, text) => `<em>${text}</em>`);
+    html = html.replace(/(\*|_)(.+)\1/gm, (_, __, text) => `<em>${text}</em>`);
     // img
-    replaced = replaced.replace(/!\[(.+)\]\((.+)\)/gm, (_, altText, imageSource) => `<img alt="${altText}" src="${imageSource}">`);
+    html = html.replace(/!\[(.+)\]\((.+)\)/gm, (_, altText, imageSource) => `<img alt="${altText}" src="${imageSource}">`);
     // a
-    replaced = replaced.replace(/\[(.+)\]\((.+)\)/gm, (_, linkText, url) => `<a href="${url}">${linkText}</a>`);
+    html = html.replace(/\[(.+)\]\((.+)\)/gm, (_, linkText, url) => `<a href="${url}">${linkText}</a>`);
     // blockquote
-    replaced = replaced.replace(/^> (.+)/gm, (_, quote) => `<blockquote>${quote}</blockquote>`);
-    htmlOutput.textContent = replaced;
-    preview.innerHTML = replaced;
-    return replaced;
+    html = html.replace(/^> (.+)/gm, (_, quote) => `<blockquote>${quote}</blockquote>`);
+    htmlOutput.textContent = html;
+    preview.innerHTML = html;
+    return html;
 }
 
 markdownInput.addEventListener('input', convertMarkdown);
