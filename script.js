e62cf6f9324e783f9a3545c5b8480b7db6ae98d4
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-23 18:33:28 +0100

    perf: move rules array outside function convertMarkdown

--- script.js
+++ script.js
@@ -2,8 +2,37 @@ const markdownInput = document.getElementById('markdown-input');
 const htmlOutput = document.getElementById('html-output');
 const preview = document.getElementById('preview');
 
+const rules = [
+    {
+        regex: /^(#{1,6}) (.+?)$/gm,
+        replace: (_, level, text) => `<h${level.length}>${text}</h${level.length}>`
+    },
+    {
+        regex: /(\*\*|__)(.+?)\1/gm,
+        replace: `<strong>$2</strong>`
+    },
+    {
+        regex: /(\*|_)(.+?)\1/gm,
+        replace: `<em>$2</em>`
+    },
+    {
+        regex: /!\[(.+?)\]\((.+?)\)/gm,
+        replace: `<img alt="$1" src="$2">`
+    },
+    {
+        regex: /\[(.+?)\]\((.+?)\)/gm,
+        replace: `<a href="$2">$1</a>`
+    },
+    {
+        regex: /^> (.+)/gm,
+        replace: `<blockquote>$1</blockquote>`
+    }
+];
+
+
 const convertMarkdown = () => {
     let html = markdownInput.value;
+
     try {
         if (html.includes('<')) throw new Error();
     } catch (Error) {
@@ -12,34 +41,8 @@ const convertMarkdown = () => {
         return;
     }
 
-    const rules = [
-        {
-            regex: /^(#+) (.+)$/gm,
-            replace: (_, level, text) => `<h${level.length}>${text}</h${level.length}>`
-        },
-        {
-            regex: /(\*\*|__)(.+)\1/gm,
-            replace: `<strong>$2</strong>`
-        },
-        {
-            regex: /(\*|_)(.+)\1/gm,
-            replace: `<em>$2</em>`
-        },
-        {
-            regex: /!\[(.+)\]\((.+)\)/gm,
-            replace: `<img alt="$1" src="$2">`
-        },
-        {
-            regex: /\[(.+)\]\((.+)\)/gm,
-            replace: `<a href="$2">$1</a>`
-        },
-        {
-            regex: /^> (.+)/gm,
-            replace: `<blockquote>$1</blockquote>`
-        }
-    ];
-
     for (const rule of rules) html = html.replace(rule.regex, rule.replace);
+
     htmlOutput.textContent = html;
     preview.innerHTML = html;
     return html;
